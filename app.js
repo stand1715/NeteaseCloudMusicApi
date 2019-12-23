223fe945ad5ef87a31961ef32cd4275a3c019496
Lee Kwok Lung <stand1715@hotmail.com>
2019-12-23 13:50:19 +0800

    update Access-Control-Allow-Headers
    update Access-Control-Allow-Headers

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
     res.set({
       'Access-Control-Allow-Credentials': true,
       'Access-Control-Allow-Origin': req.headers.origin || '*',
-      'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
+      'Access-Control-Allow-Headers': 'x-auth-token,X-Requested-With,Content-Type',
       'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
       'Content-Type': 'application/json; charset=utf-8'
     })
